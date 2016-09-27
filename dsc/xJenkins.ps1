4833b78ecc30227b49cef68b58d6f15063a9ac02
Peter Burkholder <pburkholder@pobox.com>
2016-09-27 22:44:12 +0000

    Works to install....

--- dsc/xJenkins.ps1
+++ dsc/xJenkins.ps1
@@ -1,3 +1,4 @@
+[CmdletBinding()]
 Configuration xJenkins {
   param(
     [string[]]$ComputerName,
@@ -12,7 +13,7 @@ Configuration xJenkins {
     # Set-DscLocalConfigurationManager -Path $Dsc -Verbose -Force -CimSession $cs
     LocalConfigurationManager
     {
-        DebugMode = 'All'
+        DebugMode = 'ForceModuleImport'
         RebootNodeIfNeeded = $true
     }
 
@@ -46,10 +47,35 @@ Configuration xJenkins {
       Type = "Directory"
     }
 
-    #xRemoteFile JenkinsZip {
-    #  Uri = "http://mirror.xmission.com/jenkins/windows/jenkins-${JenkinsVersion}.zip"
-    #  DestinationPath = Join-Path $Node.DSCScratchDirPath "jenkins-${JenkinsVersion}.zip"
-    #}
+    Write-Verbose "Setting up xRemoteFile"
+    $JenkinsFile = "jenkins-" + $Node.JenkinsVersion + ".zip"
+    #$JenkinsMirror = "https://ftp-nyc.osuosl.org"
+    $JenkinsURI = "http://mirror.xmission.com/jenkins/windows/" + $JenkinsFile
+    $JenkinsZip = Join-Path $Node.DSCScratchDirPath $JenkinsFile
+    xRemoteFile JenkinsZip {
+      Uri = $JenkinsURI
+      DestinationPath = $JenkinsZip
+      DependsOn = "[File]ScratchDir"
+      MatchSource = $False
+    }
+
+    Write-Verbose "Setting up Archive"
+    $JenkinsMSI = Join-Path $Node.DSCScratchDirPath "jenkins.msi"
+    Archive JenkinsMSI {
+      Path = $JenkinsZip
+      #Destination = $JenkinsMSI
+      Destination = $Node.DSCScratchDirPath
+      DependsOn = "[xRemoteFile]JenkinsZip"
+    }
+
+    Write-Verbose "Setting up Package"
+    # Use ./Get-ProductId
+    Package JenkinsPackage {
+      ProductID = "DB0F32AA-628E-4C21-AAD4-9924E24EDF75"
+      Name = "Jenkins 2.23"
+      Path = $JenkinsMSI
+      DependsOn = "[Archive]JenkinsMSI"
+    }
   }
 }
 
